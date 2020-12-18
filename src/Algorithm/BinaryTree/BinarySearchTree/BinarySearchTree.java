96e89264fd42dfb70639dfb9a88f7c5963c17889
ChuangLan <dongh@253.com>
2020-12-18 15:30:40 +0800

    查找一个数在BST中是否存在

--- src/Algorithm/BinaryTree/BinarySearchTree/BinarySearchTree.java
+++ src/Algorithm/BinaryTree/BinarySearchTree/BinarySearchTree.java
@@ -17,4 +17,22 @@ public class BinarySearchTree {
         return isValidBST(root.left, min, root)
                 && isValidBST(root.right, root, max);
     }
+
+    // 定义方法在 BST 中查找一个数是否存在
+    private boolean isInBST(TreeNode root, int target) {
+        // 只需先判断一个根节点的值是不是target，剩下的交给递归
+        if (root == null) return false;
+        if (root.val == target)
+            return true;
+
+        // 开始递归
+//        return isInBST(root.left, target)
+//                || isInBST(root.right, target);
+
+        // 上述方法虽然可行，但是没有考虑到 BST 这个“左小右大”的特性
+        if (root.val > target)
+            return isInBST(root.left, target);
+        return isInBST(root.right, target);
+    }
+
 }
