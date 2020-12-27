d129aaddfe8ccc91b864b433a810f5590002fa3f
DH <2191380@mail.deu.edu.cn>
2020-12-27 17:54:10 +0800

    二叉树遍历方式

--- src/algorithm/rinaryTree/frame/BinaryTreeTraversal.java
+++ src/algorithm/rinaryTree/frame/BinaryTreeTraversal.java
@@ -1,5 +1,7 @@
 package algorithm.rinaryTree.frame;
 
+import java.util.LinkedList;
+
 public class BinaryTreeTraversal {
     // 二叉树前序遍历
     // 代表分隔符的字符
@@ -28,5 +30,39 @@ public class BinaryTreeTraversal {
         serialize(root.right, sb);
     }
 
+    /*
+    String data = "1, 2, #, 4, #, #, 3, #, #";
+    String[] nodes = data.split(",");
+    */
+
+    /* 主函数：将字符串反序列化为二叉树结构 */
+    private TreeNode deserialize(String data) {
+        // 将字符串转换成列表
+        LinkedList<String> nodes = new LinkedList<>();
+        for (String s : data.split(SEP)) {
+            nodes.addLast(s); // 用于将对象链接到List的末尾
+        }
+        return deserialize(nodes);
+    }
+
+    /* 辅助函数：通过 nodes 列表来构造二叉树*/
+    private TreeNode deserialize(LinkedList<String> nodes) {
+        if (nodes.isEmpty()) return null;
+
+        /* 前序遍历 */
+        // nodes列表的最左侧就是根节点
+        String first = nodes.removeFirst();
+        if (first.equals(null)) return null;
+
+        // Integer.parseInt(String)的作用就是将String字符类型数据转换为Integer整型数据。
+        TreeNode root = new TreeNode(Integer.parseInt(first));
+        /* 分隔线 */
+
+        // 其余节点交给递归
+        root.left = deserialize(nodes);
+        root.right = deserialize(nodes);
+
+        return root;
+    }
 
 }
