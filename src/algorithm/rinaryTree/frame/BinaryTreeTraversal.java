7adeceb2fe14850406d43c37bac174480e63109c
DH <2191380@mail.deu.edu.cn>
2020-12-27 18:46:41 +0800

    二叉树遍历方式

--- src/algorithm/rinaryTree/frame/BinaryTreeTraversal.java
+++ src/algorithm/rinaryTree/frame/BinaryTreeTraversal.java
@@ -3,21 +3,21 @@ package algorithm.rinaryTree.frame;
 import java.util.LinkedList;
 
 public class BinaryTreeTraversal {
-    // 二叉树前序遍历
+    /* 二叉树前序遍历 */
     // 代表分隔符的字符
     String SEP = ",";
     // 代表 null 指针的字符
     String NULL = "#";
 
     /*主函数：将二叉树序列化为字符串*/
-    private String serialize(TreeNode root) {
+    private String freSerialize(TreeNode root) {
         StringBuilder sb = new StringBuilder();
-        serialize(root, sb);
+        freSerialize(root, sb);
         return sb.toString();
     }
 
     /*辅助函数：将二叉树存入StringBuilder*/
-    private void serialize(TreeNode root, StringBuilder sb) {
+    private void freSerialize(TreeNode root, StringBuilder sb) {
         if (root == null) {
             sb.append(NULL).append(SEP);
             return;
@@ -26,8 +26,8 @@ public class BinaryTreeTraversal {
         // 进行前序遍历
         sb.append(root.val).append(SEP);
 
-        serialize(root.left, sb);
-        serialize(root.right, sb);
+        freSerialize(root.left, sb);
+        freSerialize(root.right, sb);
     }
 
     /*
@@ -36,17 +36,17 @@ public class BinaryTreeTraversal {
     */
 
     /* 主函数：将字符串反序列化为二叉树结构 */
-    private TreeNode deserialize(String data) {
+    private TreeNode deFreSerialize(String data) {
         // 将字符串转换成列表
         LinkedList<String> nodes = new LinkedList<>();
         for (String s : data.split(SEP)) {
             nodes.addLast(s); // 用于将对象链接到List的末尾
         }
-        return deserialize(nodes);
+        return deFreSerialize(nodes);
     }
 
     /* 辅助函数：通过 nodes 列表来构造二叉树*/
-    private TreeNode deserialize(LinkedList<String> nodes) {
+    private TreeNode deFreSerialize(LinkedList<String> nodes) {
         if (nodes.isEmpty()) return null;
 
         /* 前序遍历 */
@@ -59,10 +59,31 @@ public class BinaryTreeTraversal {
         /* 分隔线 */
 
         // 其余节点交给递归
-        root.left = deserialize(nodes);
-        root.right = deserialize(nodes);
+        root.left = deFreSerialize(nodes);
+        root.right = deFreSerialize(nodes);
 
         return root;
     }
 
+    /* 后续遍历 */
+    /*主函数：将二叉树序列化为字符串*/
+    private String postorderSerialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        postorderSerialize(root, sb);
+        return sb.toString();
+    }
+
+    /* 副函数：将二叉树序列化为字符串 */
+    private void postorderSerialize(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append(NULL).append(SEP);
+            return;
+        }
+
+        // 后序遍历是先递归再进行转化为字符串
+        postorderSerialize(root.left, sb);
+        postorderSerialize(root.right, sb);
+
+        sb.append(root.val).append(SEP);
+    }
 }
