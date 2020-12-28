5370f98cde36f0972c8f1d05b06ff5d269304082
ChuangLan <dongh@253.com>
2020-12-28 20:12:48 +0800

    二叉树遍历

--- src/algorithm/rinaryTree/frame/BinaryTreeTraversal.java
+++ src/algorithm/rinaryTree/frame/BinaryTreeTraversal.java
@@ -89,5 +89,29 @@ public class BinaryTreeTraversal {
     }
 
     /* 二叉树后序遍历的反序列化 */
+    /* 主函数： 将字符串反序列化为二叉树结构 */
+    private TreeNode postorderDeserialize(String data) {
+        LinkedList<String> nodes = new LinkedList<>();
+        for (String s : data.split(SEP)) {
+            nodes.addLast(s);
+        }
+        return postorderDeserialize(nodes);  // 调用副函数
+    }
 
+    /* 副函数：通过 nodes 列表构造二叉树 */
+    private TreeNode postorderDeserialize(LinkedList<String> nodes) {
+        if (nodes.isEmpty()) return null;
+
+        /* 反序列化位置 */
+        // 从后往前取出元素
+        String last = nodes.getLast();
+        if (last.equals(null)) return null;
+        TreeNode root = new TreeNode(Integer.parseInt(last));
+
+        // 先构造右子树，后构造左子树
+        root.right = postorderDeserialize(nodes);
+        root.left = postorderDeserialize(nodes);
+
+        return root;
+    }
 }
