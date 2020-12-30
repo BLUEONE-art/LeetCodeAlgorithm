0ed0939bdae137fe3f82c5dcfe8296619e509e77
ChuangLan <dongh@253.com>
2020-12-30 20:20:26 +0800

    git公共祖先

--- src/algorithm/rinaryTree/lcaFrame/LowestCommonAncestor.java
+++ src/algorithm/rinaryTree/lcaFrame/LowestCommonAncestor.java
@@ -2,4 +2,25 @@ package algorithm.rinaryTree.lcaFrame;
 
 public class LowestCommonAncestor {
 
+    private TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // 基本情况
+        if (root == null) return null;
+        if (root == p || root == q) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        // 相当于二叉树的后序遍历
+        // 情况1
+        if (left != null && right != null) {
+            return root;
+        }
+        // 情况2
+        if (left == null && right == null) {
+            return null;
+        }
+        // 情况3
+        return left == null ? right : left;
+    }
+
 }
