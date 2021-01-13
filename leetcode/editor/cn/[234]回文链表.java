bf02ee0d1cf94dde905fb08981f994480da1056f
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-01-13 14:28:33 +0800

    刷题

---
+++ leetcode/editor/cn/[234]回文链表.java
@@ -0,0 +1,34 @@
+//请判断一个链表是否为回文链表。 
+//
+// 示例 1: 
+//
+// 输入: 1->2
+//输出: false 
+//
+// 示例 2: 
+//
+// 输入: 1->2->2->1
+//输出: true
+// 
+//
+// 进阶： 
+//你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？ 
+// Related Topics 链表 双指针 
+// 👍 817 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
