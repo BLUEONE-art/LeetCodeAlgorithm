2e22f09d15dfaf17201c10f0a80479323fdfb4ce
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-01-13 14:12:06 +0800

    刷题

--- leetcode/editor/cn/[125]验证回文串.java
+++ leetcode/editor/cn/[125]验证回文串.java
@@ -19,8 +19,32 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    // 核心思想：判断是不是回文串，只需从两端的元素进行比较，只要有一个不同，则返回 false，否则返回 true。
     public boolean isPalindrome(String s) {
+        // 先将字符串统一变成小写
+        String str = s.toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
 
+        // base case
+        if (s == null || s.length() == 0) return true;
+
+        // 注意循环条件
+        while (left < right) {
+            // 假如字符串中左边存在除了字母和数字以外的符号，让 left++;
+            while (left < right && !Character.isLetterOrDigit(str.charAt(left))) {
+                left++;
+            }
+
+            // 假如字符串中右边存在除了字母和数字以外的符号，让 right--;
+            while (left < right && !Character.isLetterOrDigit(str.charAt(right))) {
+                right--;
+            }
+
+            if (str.charAt(left) != str.charAt(right)) return false;
+            left++; right--;
+        }
+        return true;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
