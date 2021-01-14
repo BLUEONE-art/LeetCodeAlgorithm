9c618f74a4c23b4e354bad360ec0c7c3057eedfd
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-01-14 16:05:21 +0800

    刷题

--- leetcode_binarytree/leetcode/editor/cn/[226]翻转二叉树.java
+++ leetcode_binarytree/leetcode/editor/cn/[226]翻转二叉树.java
@@ -37,8 +37,21 @@
  * }
  */
 class Solution {
+    // 反转二叉树的关键思路：只需要将二叉树上的每个节点的左右子节点互换即可
     public TreeNode invertTree(TreeNode root) {
 
+        // base case
+        if (root == null) return null;
+
+        // 前序遍历的位置
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;    // base case + 前序遍历代码都是 root 节点要完成的事，剩下的交给递归
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
