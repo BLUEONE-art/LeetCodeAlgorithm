7c4cc75d9638a0c7fd8c924debdcdf4a993ceafd
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-01-14 18:11:43 +0800

    刷题

---
+++ leetcode_binarytree/leetcode/editor/cn/[114]二叉树展开为链表.java
@@ -0,0 +1,51 @@
+//给定一个二叉树，原地将它展开为一个单链表。 
+//
+// 
+//
+// 例如，给定二叉树 
+//
+//     1
+//   / \
+//  2   5
+// / \   \
+//3   4   6 
+//
+// 将其展开为： 
+//
+// 1
+// \
+//  2
+//   \
+//    3
+//     \
+//      4
+//       \
+//        5
+//         \
+//          6 
+// Related Topics 树 深度优先搜索 
+// 👍 683 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
