dfefb16b9e3014aed06e43f75c7362c840929f8f
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-01-14 18:26:32 +0800

    刷题

--- leetcode_binarytree/leetcode/editor/cn/[114]二叉树展开为链表.java
+++ leetcode_binarytree/leetcode/editor/cn/[114]二叉树展开为链表.java
@@ -45,7 +45,36 @@
  */
 class Solution {
     public void flatten(TreeNode root) {
-        
+
+        // base case
+        if (root == null) return;
+
+        // 后序遍历
+        // 不需要管怎么才能把二叉树的左右子树拉直成一个链表，只需要递归调用这个函数即可
+        flatten(root.left);
+        flatten(root.right);
+
+        // 执行完上述递归代码后，我们只需要知道此时二叉树的左右子树已经被拉直了
+        // 考虑如何将 root 先拼接左子树，再在其基础上拼接右子树
+        // 新定义两个 TreeNode 分别存放左子树和右子树
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        // 清零 root 原先的左子树
+        root.left = null;
+        // 让 root 的下一个节点指向左子树的头节点
+        root.right = left;
+
+        // 将右子树拼接到上述的结果中
+        // 在此之前需要获得拼接完左子树后的 root 的最后一个节点
+        // 定义一个 p 复制 root，用它来计算最后一个节点
+        TreeNode p = root;
+        while (p.right != null) {
+            p = p.right;
+        }
+
+        // 再让 p 的指向右子树的头节点即可
+        p.right = right;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
