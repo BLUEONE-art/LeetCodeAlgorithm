2be02aca16ce2ee80ce1fe04491df2628d7c9909
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-01-16 14:40:03 +0800

    刷题

---
+++ leetcode_binarytree/leetcode/editor/cn/[98]验证二叉搜索树.java
@@ -0,0 +1,69 @@
+//给定一个二叉树，判断其是否是一个有效的二叉搜索树。 
+//
+// 假设一个二叉搜索树具有如下特征： 
+//
+// 
+// 节点的左子树只包含小于当前节点的数。 
+// 节点的右子树只包含大于当前节点的数。 
+// 所有左子树和右子树自身必须也是二叉搜索树。 
+// 
+//
+// 示例 1: 
+//
+// 输入:
+//    2
+//   / \
+//  1   3
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入:
+//    5
+//   / \
+//  1   4
+//     / \
+//    3   6
+//输出: false
+//解释: 输入为: [5,1,4,null,null,3,6]。
+//     根节点的值为 5 ，但是其右子节点值为 4 。
+// 
+// Related Topics 树 深度优先搜索 递归 
+// 👍 890 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+
+        return isValidBST(root, null, null);
+    }
+
+    public boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
+
+        // base case
+        if (root == null) return true;
+        if (min != null && root.val <= min.val) return false;
+        if (max != null && root.val >= max.val) return false;
+
+        return isValidBST(root.left, min, root)
+                && isValidBST(root.right, root, max);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
