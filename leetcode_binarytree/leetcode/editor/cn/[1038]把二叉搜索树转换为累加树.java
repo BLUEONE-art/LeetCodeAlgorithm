e94e0f9bce4caad05860878169541efbfd6cd1d2
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-01-16 10:14:34 +0800

    刷题

---
+++ leetcode_binarytree/leetcode/editor/cn/[1038]把二叉搜索树转换为累加树.java
@@ -0,0 +1,103 @@
+//给出二叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树（Greater Sum Tree），使每个节点 node 的新值等于原树中大于或等于
+// node.val 的值之和。 
+//
+// 提醒一下，二叉搜索树满足下列约束条件： 
+//
+// 
+// 节点的左子树仅包含键 小于 节点键的节点。 
+// 节点的右子树仅包含键 大于 节点键的节点。 
+// 左右子树也必须是二叉搜索树。 
+// 
+//
+// 注意：该题目与 538: https://leetcode-cn.com/problems/convert-bst-to-greater-tree/ 相同
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 输入：[4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+//输出：[30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+// 
+//
+// 示例 2： 
+//
+// 输入：root = [0,null,1]
+//输出：[1,null,1]
+// 
+//
+// 示例 3： 
+//
+// 输入：root = [1,0,2]
+//输出：[3,3,2]
+// 
+//
+// 示例 4： 
+//
+// 输入：root = [3,2,4,1]
+//输出：[7,9,4,10]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中的节点数介于 1 和 100 之间。 
+// 每个节点的值介于 0 和 100 之间。 
+// 树中的所有值 互不相同 。 
+// 给定的树为二叉搜索树。 
+// 
+// Related Topics 二叉搜索树 
+// 👍 102 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode bstToGst(TreeNode root) {
+
+        // traverse() 返回 root
+        return traverse(root);
+    }
+
+    // 定义一个 sum 记录每次遍历后降序求和的结果
+    // 比如：第一个 sum 就是最大节点的值，第二个 sum 是最大和第二大节点值的和，以此类推...
+    // 每一个 sum 赋值给当前 root 节点
+    int sum = 0;
+
+    public TreeNode traverse(TreeNode root) {
+
+        // base case
+        if (root == null) return null;
+
+        // 二叉搜索树的中序遍历
+        // 正常情况：从小到大，升序
+        // 先递归右子树：从大到小，降序
+        traverse(root.right);
+
+        sum += root.val;
+        root.val = sum;
+
+        traverse(root.left);
+
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
