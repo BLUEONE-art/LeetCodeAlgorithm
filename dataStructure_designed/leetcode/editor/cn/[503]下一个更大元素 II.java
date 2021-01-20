f2a62565db6ef00b36e59c19789d9a0567a56f68
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-01-20 11:39:09 +0800

    刷题

--- dataStructure_designed/leetcode/editor/cn/[503]下一个更大元素 II.java
+++ dataStructure_designed/leetcode/editor/cn/[503]下一个更大元素 II.java
@@ -18,8 +18,37 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    /* 寻找下一个更大的元素(循环数组) */
     public int[] nextGreaterElements(int[] nums) {
 
+        // nums 数组的大小
+        int size = nums.length;
+
+        // 装结果
+        int[] res = new int[size];
+        // 装比较的结果，即不断更新的"更大的数"
+        Stack<Integer> stack = new Stack<>();
+
+        // 倒着将 nums 数组中的元素放到栈中
+        // 利用取模的方法可以使数组在不扩容的情况下实现增倍
+        // 假装已经扩容了一倍
+        for (int i = 2 * size - 1; i >= 0; i--) {
+
+            // ②通过循环比较元素，更新栈中的“更大的元素”
+            while (!stack.isEmpty() && nums[i % size] >= stack.peek()) {
+
+                // 找到了更大的元素，栈中较小的元素就可以舍去
+                stack.pop();
+            }
+
+            // ③找到了当前元素后第一个比它大的数，更新 res
+            res[i % size] = stack.isEmpty() ? -1 : stack.peek();
+
+            // ①放入数组元素
+            stack.push(nums[i % size]);
+        }
+
+        return res;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
