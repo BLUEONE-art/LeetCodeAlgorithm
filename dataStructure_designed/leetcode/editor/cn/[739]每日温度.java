5c8d2e8512b38e479e1b19bd3d8240b95aa1baa5
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-01-20 10:25:57 +0800

    刷题

--- dataStructure_designed/leetcode/editor/cn/[739]每日温度.java
+++ dataStructure_designed/leetcode/editor/cn/[739]每日温度.java
@@ -11,8 +11,33 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    /* 寻找下一个更大的温度 */
     public int[] dailyTemperatures(int[] T) {
 
+        // 装结果
+        int[] res = new int[T.length];
+        // 装索引
+        Stack<Integer> stack = new Stack<>();
+
+        // 将数组 T 中元素索引倒着入栈
+        for (int i = T.length - 1; i >= 0; i--) {
+
+            // ②循环进行元素大小比较
+            // 如果栈中元素不为空，说明此时有比当前元素大的值
+            // 如果战争没有元素，说明该元素后面没有比他大的
+            while (!stack.isEmpty() && T[i] >= T[stack.peek()]) {
+
+                // 如果栈中索引对应的值没有当前 T 数组中的元素大，那栈中索引对应的值可以“走了”
+                stack.pop();
+            }
+
+            // ③将比较结果放入 res
+            res[i] = stack.isEmpty() ? 0 : stack.peek() - i;
+
+            // ①元素索引入栈
+            stack.push(i);
+        }
+        return res;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
