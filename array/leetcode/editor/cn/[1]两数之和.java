bea4e8eadd6eea3292e585af15d693164fc64ec7
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-01-22 16:53:28 +0800

    刷题

---
+++ array/leetcode/editor/cn/[1]两数之和.java
@@ -0,0 +1,84 @@
+//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。 
+//
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。 
+//
+// 你可以按任意顺序返回答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [2,7,11,15], target = 9
+//输出：[0,1]
+//解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,4], target = 6
+//输出：[1,2]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [3,3], target = 6
+//输出：[0,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 2 <= nums.length <= 103 
+// -109 <= nums[i] <= 109 
+// -109 <= target <= 109 
+// 只会存在一个有效答案 
+// 
+// Related Topics 数组 哈希表 
+// 👍 10122 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    // 有序数组就要想到左右指针的方法
+    public int[] twoSum(int[] nums, int target) {
+        int[] arr = Arrays.copyOfRange(nums, 0, nums.length);
+//        int[] arr = new int[nums.length];
+//        for (int i = 0; i < nums.length; i++) {
+//            arr[i] = nums[i];
+//        }
+        Arrays.sort(arr);
+        int left = 0; // 左侧最小索引
+        int right = arr.length - 1; // 搜索空间：[left, right]
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == target) {
+                int a = findIndex(left, arr, nums);
+                int b = findIndex(right, arr, nums);
+                return new int[]{a, b};
+            } else if (sum < target) {
+                left++; // 让 left 大一点再搜索
+            } else if (sum > target) {
+                right--; // right 小一点再搜索
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    // 找到 left 和 right 在 nums 数组中的索引
+    private int findIndex(int afterIndex, int[] arr, int[] nums) {
+        int orignalIndex = 0, i = 0;
+        while (i < nums.length) {
+            if (arr[afterIndex] == nums[i]) {
+                orignalIndex = i;
+            }
+            i++;
+        }
+        return orignalIndex;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
