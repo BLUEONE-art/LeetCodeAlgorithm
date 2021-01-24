f705760a1699199a29cd13fdc7796bd50b84e2be
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-01-24 19:20:10 +0800

    刷题

--- array/leetcode/editor/cn/[380]常数时间插入、删除和获取随机元素.java
+++ array/leetcode/editor/cn/[380]常数时间插入、删除和获取随机元素.java
@@ -40,110 +40,107 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 /* 以 O(1) 时间复杂度完成插入、删除、随机获取元素的操作*/
 class RandomizedSet {
-//    // 想要以 O(1) 随机访问元素，必选数组实现
-//    ArrayList<Integer> nums;
-//    // 创建 HashMap 记录数组元素对应的索引值
-//    HashMap<Integer, Integer> valToIndex;
-//    /** Initialize your data structure here. */
-//    public RandomizedSet() {
-//        this.nums = new ArrayList<>();
-//        this.valToIndex = new HashMap<>();
-//    }
-//
-//    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-//    public boolean insert(int val) {
-//        // 如果数组中存在 val，则返回false
-//        if (valToIndex.containsKey(val)) return false;
-//        // 想要以 O(1) 复杂度插入元素，为了避免移动元素，必须将元素插入至数组末尾
-//        // 没加任何数之前，nums.size() == 0
-//        valToIndex.put(val, nums.size());
-//        nums.add(val);
-//        return true;
-//    }
-//
-//    /** Removes a value from the set. Returns true if the set contained the specified element. */
-//    public boolean remove(int val) {
-//        // 如果元素不存在 val，返回false
-//        if (!valToIndex.containsKey(val)) return false;
-//        // 想要以 O(1) 复杂度移除元素，先将要删除元素移动至数组末尾，再删除
-//        int index = valToIndex.get(val); // 获取 val 的索引 index
-//        // 将 valToIndex 中最后元素(即 nums 里最后的元素)索引换成 index
-//        valToIndex.put(nums.get(nums.size() - 1), index);
-//        // 将数组中 val 和 最后的元素互换位置, 此时 val 就是最后的元素
-//        Collections.swap(nums, index, nums.size() - 1);
-//        // 删除数组最后一个元素，即为 val
-//        nums.remove(nums.size() - 1);
-//        // 删除 valToIndex 中 val 对应的索引
-//        valToIndex.remove(val);
-//        return true;
-//    }
-//
-//    /** Get a random element from the set. */
-//    public int getRandom() {
-//        int randomIndex = (int)Math.random() * nums.size();
-//        while (nums.get(randomIndex) == null && nums.size() > 0) {
-//            randomIndex = (int)Math.random() * nums.size();
-//        }
-//        return nums.get(randomIndex);
-//    }
-
-    private Integer[] table = null;
-    private int size = 0;
-
+    // 想要以 O(1) 随机访问元素，必选数组实现
+    ArrayList<Integer> nums;
+    // 创建 HashMap 记录数组元素对应的索引值
+    HashMap<Integer, Integer> valToIndex;
     /** Initialize your data structure here. */
     public RandomizedSet() {
-        table = new Integer[2];
+        this.nums = new ArrayList<>();
+        this.valToIndex = new HashMap<>();
     }
 
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public boolean insert(int val) {
-        Integer v = new Integer(val);
-        int index;
-        if (table[index = (v.hashCode()& (table.length-1))] == null) {
-            table[index] = v;
-            size++;
-            return true;
-        } else if (table[index] != val){
-            //扩容然后再插
-            reSize();
-            return insert(val);
-        }
-        return false;
-    }
-
-    private boolean reSize() {
-        Integer[] oldTable, newTable;
-        if ((oldTable = table) != null) {
-            newTable = new Integer[oldTable.length << 1];
-            for (int i = 0; i < oldTable.length; i++) {
-                if (oldTable[i] != null)
-                    newTable[oldTable[i].hashCode()&(newTable.length-1)] = oldTable[i];
-            }
-            this.table = newTable;
-            return true;
-        }
-        return false;
+        // 如果数组中存在 val，则返回false
+        if (valToIndex.containsKey(val)) return false;
+        // 想要以 O(1) 复杂度插入元素，为了避免移动元素，必须将元素插入至数组末尾
+        // 没加任何数之前，nums.size() == 0
+        valToIndex.put(val, nums.size());
+        nums.add(val);
+        return true;
     }
 
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
-        int index;
-        if (table[index = (Integer.hashCode(val)&(table.length-1))] != null && table[index] == val) {
-            table[index] = null;
-            size--;
-            return true;
-        }
-        return false;
+        // 如果元素不存在 val，返回false
+        if (!valToIndex.containsKey(val)) return false;
+        // 想要以 O(1) 复杂度移除元素，先将要删除元素移动至数组末尾，再删除
+        int index = valToIndex.get(val); // 获取 val 的索引 index
+        // 将 valToIndex 中最后元素(即 nums 里最后的元素)索引换成 index
+        valToIndex.put(nums.get(nums.size() - 1), index);
+        // 将数组中 val 和 最后的元素互换位置, 此时 val 就是最后的元素
+        Collections.swap(nums, index, nums.size() - 1);
+        // 删除数组最后一个元素，即为 val
+        nums.remove(nums.size() - 1);
+        // 删除 valToIndex 中 val 对应的索引
+        valToIndex.remove(val);
+        return true;
     }
 
     /** Get a random element from the set. */
     public int getRandom() {
-        int index = (int)(Math.random()*(table.length));
-        while(table[index] == null && size > 0){
-            index = (int)(Math.random()*(table.length));
-        }
-        return table[index];
+        return nums.get((int)(Math.random() * nums.size()));
     }
+
+//    private Integer[] table = null;
+//    private int size = 0;
+//
+//    /** Initialize your data structure here. */
+//    public RandomizedSet() {
+//        table = new Integer[2];
+//    }
+//
+//    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+//    public boolean insert(int val) {
+//        Integer v = new Integer(val);
+//        int index;
+//        if (table[index = (v.hashCode()& (table.length-1))] == null) {
+//            table[index] = v;
+//            size++;
+//            return true;
+//        } else if (table[index] != val){
+//            //扩容然后再插
+//            reSize();
+//            return insert(val);
+//        }
+//        return false;
+//    }
+//
+//    private boolean reSize() {
+//        Integer[] oldTable, newTable;
+//        if ((oldTable = table) != null) {
+//            newTable = new Integer[oldTable.length << 1];
+//            for (int i = 0; i < oldTable.length; i++) {
+//                if (oldTable[i] != null)
+//                    newTable[oldTable[i].hashCode()&(newTable.length-1)] = oldTable[i];
+//            }
+//            this.table = newTable;
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    /** Removes a value from the set. Returns true if the set contained the specified element. */
+//    public boolean remove(int val) {
+//        int index;
+//        if (table[index = (Integer.hashCode(val)&(table.length-1))] != null && table[index] == val) {
+//            table[index] = null;
+//            size--;
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    /** Get a random element from the set. */
+//    public int getRandom() {
+//        int index = (int)(Math.random()*(table.length));
+//        while(table[index] == null && size > 0){
+//            index = (int)(Math.random()*(table.length));
+//        }
+////        return table[index];
+//        return index;
+//    }
 }
 
 /**
