f4154a3834e02fcf1e4e06c6a71b173bb380968c
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-01-31 16:30:48 +0800

    刷题

--- DP/leetcode/editor/cn/[53]最大子序和.java
+++ DP/leetcode/editor/cn/[53]最大子序和.java
@@ -19,8 +19,21 @@ class Solution {
     /* 最大子序和问题 */
     public int maxSubArray(int[] nums) {
         // dp[i] 数组：数组以 nums[i] 结尾的最大字序和
-        int n = nums
-        int[] dp = new int[]
+        int n = nums.length;
+        if (n == 0) return 0;
+        int[] dp = new int[n];
+        // base case：数组第一位 nums[0] = dp[0]
+        dp[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            // dp[i] 表示 dp[i - 1] 如果跟 nums[i] 相加形成一个更大的子数组的话，就返回这个合并数组
+            // 如果合并数组还没有自己本身 nums[i] 的数值大，直接返回 nums[i] 即可
+            dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
+        }
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
