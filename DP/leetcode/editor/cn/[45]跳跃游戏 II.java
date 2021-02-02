30ed7da14e99215cd6b77ede53ca02728ef4a8da
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-02-02 14:43:53 +0800

    刷题

--- DP/leetcode/editor/cn/[45]跳跃游戏 II.java
+++ DP/leetcode/editor/cn/[45]跳跃游戏 II.java
@@ -21,39 +21,39 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-//    // 现在是一定可以到达数组的最后一位，要求出到最后一位最小要跳的次数
-//    // 动态规划
-//    public int jump(int[] nums) {
-//        // 创建备忘录放每一个子问题的结果
-//        int n = nums.length;
-//        // 因为从 0 到 n - 1(末尾位置) 最多走 n - 1 步，初始化要一个不在选择范围的数
-//        int[] memo = new int[n];
-//        Arrays.fill(memo, n);
-//        return dp(memo, nums, 0);
-//    }
-//    // dp(nums, p)：表示从 p 位置开始跳到末尾最少用的步数为 dp(nums, p)
-//    // 我们要求的就是 dp(nums, 0)：从 0 位置开始跳到末尾的最小步数
-//    private int dp(int[] memo, int[] nums, int p) {
-//        int n = nums.length;
-//        // base case：如果 p 已经超过或等于最后一位的位置(n - 1)，需要 0 步
-//        if (p >= n - 1) return 0;
-//        // 如果备忘录里有结果，直接返回
-//        if (memo[p] != n) {
-//            return memo[p];
-//        }
-//        // 循环遍历递归
-//        // 在 p 位置可以走 steps 步
-//        int steps = nums[p];
-//        // 我选择可以走 1 ~ steps 步
-//        for (int i = 1; i <= steps; i++) {
-//            // 递归到子问题 dp(memo, nums, p + i)
-//            // 转换为从 p 跳到末尾 --> p + i 位置跳到末尾
-//            int subProblem = dp(memo, nums, p + i);
-//            // 找到所有可能 (p + 1 ~ steps) 中最小次数的结果存入 memo
-//            memo[p] = Math.min(memo[p], subProblem + 1); // +1 表示在 p 位置跳到 p + i 算一次
-//        }
-//        return memo[p];
-//    }
+    // 现在是一定可以到达数组的最后一位，要求出到最后一位最小要跳的次数
+    // 动态规划
+    public int jump(int[] nums) {
+        // 创建备忘录放每一个子问题的结果
+        int n = nums.length;
+        // 因为从 0 到 n - 1(末尾位置) 最多走 n - 1 步，初始化要一个不在选择范围的数
+        int[] memo = new int[n];
+        Arrays.fill(memo, n);
+        return dp(memo, nums, 0);
+    }
+    // dp(nums, p)：表示从 p 位置开始跳到末尾最少用的步数为 dp(nums, p)
+    // 我们要求的就是 dp(nums, 0)：从 0 位置开始跳到末尾的最小步数
+    private int dp(int[] memo, int[] nums, int p) {
+        int n = nums.length;
+        // base case：如果 p 已经超过或等于最后一位的位置(n - 1)，需要 0 步
+        if (p >= n - 1) return 0;
+        // 如果备忘录里有结果，直接返回
+        if (memo[p] != n) {
+            return memo[p];
+        }
+        // 循环遍历递归
+        // 在 p 位置可以走 steps 步
+        int steps = nums[p];
+        // 我选择可以走 1 ~ steps 步
+        for (int i = 1; i <= steps; i++) {
+            // 递归到子问题 dp(memo, nums, p + i)
+            // 转换为从 p 跳到末尾 --> p + i 位置跳到末尾
+            int subProblem = dp(memo, nums, p + i);
+            // 找到所有可能 (p + 1 ~ steps) 中最小次数的结果存入 memo
+            memo[p] = Math.min(memo[p], subProblem + 1); // +1 表示在 p 位置跳到 p + i 算一次
+        }
+        return memo[p];
+    }
 
     public int jump(int[] nums) {
         // 动态规划的时候是在 p 位置计算所有可能的子问题，在子问题中选择步数最小的
