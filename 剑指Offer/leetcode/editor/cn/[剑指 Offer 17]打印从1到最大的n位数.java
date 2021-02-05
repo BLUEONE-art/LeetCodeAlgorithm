8187939b14bed9161d8e3b5268ffd83e65d638b4
Dong Hao <2191380@mail.dhu.edu.cn>
2021-02-05 22:47:36 +0800

    刷题

---
+++ 剑指Offer/leetcode/editor/cn/[剑指 Offer 17]打印从1到最大的n位数.java
@@ -0,0 +1,33 @@
+//输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。 
+//
+// 示例 1: 
+//
+// 输入: n = 1
+//输出: [1,2,3,4,5,6,7,8,9]
+// 
+//
+// 
+//
+// 说明： 
+//
+// 
+// 用返回一个整数列表来代替打印 
+// n 为正整数 
+// 
+// Related Topics 数学 
+// 👍 80 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] printNumbers(int n) {
+        int size = (int)Math.pow(10, n);
+        int[] res = new int[size - 1];
+        // 1：表示输出 [1, 10)
+        for (int i = 1; i < size; i++) {
+            res[i - 1] = i;
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
