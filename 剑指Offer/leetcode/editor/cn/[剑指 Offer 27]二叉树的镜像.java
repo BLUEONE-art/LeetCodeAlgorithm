690210e1c36681b7e4ef3737646d61acaf8a5898
Dong Hao <2191380@mail.dhu.edu.cn>
2021-02-08 23:10:35 +0800

    刷题

---
+++ 剑指Offer/leetcode/editor/cn/[剑指 Offer 27]二叉树的镜像.java
@@ -0,0 +1,65 @@
+//请完成一个函数，输入一个二叉树，该函数输出它的镜像。 
+//
+// 例如输入： 
+//
+// 4 
+// / \ 
+// 2 7 
+// / \ / \ 
+//1 3 6 9 
+//镜像输出： 
+//
+// 4 
+// / \ 
+// 7 2 
+// / \ / \ 
+//9 6 3 1 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：root = [4,2,7,1,3,6,9]
+//输出：[4,7,2,9,6,3,1]
+// 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 节点个数 <= 1000 
+//
+// 注意：本题与主站 226 题相同：https://leetcode-cn.com/problems/invert-binary-tree/ 
+// Related Topics 树 
+// 👍 99 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode mirrorTree(TreeNode root) {
+        // 二叉树前序遍历，将每个根节点的两个子节点位置互换
+        // base case
+        if (root == null) return null;
+
+        // 前序遍历代码位置
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        // 前序遍历
+        mirrorTree(root.left);
+        mirrorTree(root.right);
+
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
