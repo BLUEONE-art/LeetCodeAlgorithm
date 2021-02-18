0d7bcb53f7e5ef9bf224b2521f482f50f10a3277
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-02-18 09:56:17 +0800

    刷题

---
+++ 剑指Offer/leetcode/editor/cn/[剑指 Offer 54]二叉搜索树的第k大节点.java
@@ -0,0 +1,65 @@
+//给定一棵二叉搜索树，请找出其中第k大的节点。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: root = [3,1,4,null,2], k = 1
+//   3
+//  / \
+// 1   4
+//  \
+//   2
+//输出: 4 
+//
+// 示例 2: 
+//
+// 输入: root = [5,3,6,2,4,null,null,1], k = 3
+//       5
+//      / \
+//     3   6
+//    / \
+//   2   4
+//  /
+// 1
+//输出: 4 
+//
+// 
+//
+// 限制： 
+//
+// 1 ≤ k ≤ 二叉搜索树元素个数 
+// Related Topics 树 
+// 👍 118 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // 核心思想：中序遍历的倒序为递减序列
+    int res, k;
+    public int kthLargest(TreeNode root, int k) {
+        this.k = k;
+        dfs(root);
+        return res;
+    }
+    public void dfs(TreeNode root) {
+        // base case
+        if (root == null) return;
+        // 中序遍历倒序
+        dfs(root.right);
+        // 在这进行中序遍历的操作
+        if (k == 0) return;
+        if (--k == 0) res = root.val;
+        dfs(root.left);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
