aa77a997eb1b3c7e4f980245cb99b69f00047b47
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-02-20 10:25:27 +0800

    刷题

--- 剑指Offer/leetcode/editor/cn/[剑指 Offer 62]圆圈中最后剩下的数字.java
+++ 剑指Offer/leetcode/editor/cn/[剑指 Offer 62]圆圈中最后剩下的数字.java
@@ -32,9 +32,47 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    // i = 1 开始递推
+    // 递推公式：f(i) = (f(i - 1) + m) % i
+    public int lastRemaining(int n, int m) {
+        // 相当于 dp[0] == 0，后面要求 dp[1]，dp[2]，dp[3]，dp[4]
+        int x = 0;
+        for (int i = 2; i <= n; i++) {
+            x = (x + m) % i;
+        }
+        return x;
+    }
+
+    public int lastRemaining(int n, int m) {
+        // 迭代，动态规划
+        int[] dp = new int[n];
+        if (n == 1) dp[0] = 0;
+        for (int i = 2; i <= n; i++) {
+            dp[i - 1] = (dp[i - 2] + m) % i;
+        }
+        return dp[n - 1];
+    }
+
+    // 递归公式：f(n) = (f(n - 1) + m) % n
     public int lastRemaining(int n, int m) {
         if (n == 1) return 0;
         return (lastRemaining(n - 1, m) + m) % n;
+    }
+
+    // 没有分支，其实没有降低时间复杂度
+    public int lastRemaining(int n, int m) {
+        // 备忘录
+        ArrayList<Integer> memo = new ArrayList<>(Collections.nCopies(n + 1, -1));
+        return dp(memo, n, m);
+    }
+    private int dp(ArrayList<Integer> memo, int n, int m) {
+        // base case
+        if (n == 1) return 0;
+        // 如果备忘录中有，直接返回
+        if (memo.get(n) >= 0) return memo.get(n);
+        // 若没有，再计算
+        memo.set(n, (dp(memo, n - 1, m) + m) % n);
+        return memo.get(n);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
