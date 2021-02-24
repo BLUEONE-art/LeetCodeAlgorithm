66aecf35c318d1c63b4730b9f0b3c20de0639359
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-02-24 15:02:36 +0800

    刷题

---
+++ 剑指Offer/leetcode/editor/cn/[剑指 Offer 35]复杂链表的复制.java
@@ -0,0 +1,94 @@
+//请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指
+//向链表中的任意节点或者 null。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+//输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 输入：head = [[1,1],[2,1]]
+//输出：[[1,1],[2,1]]
+// 
+//
+// 示例 3： 
+//
+// 
+//
+// 输入：head = [[3,null],[3,0],[3,null]]
+//输出：[[3,null],[3,0],[3,null]]
+// 
+//
+// 示例 4： 
+//
+// 输入：head = []
+//输出：[]
+//解释：给定的链表为空（空指针），因此返回 null。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// -10000 <= Node.val <= 10000 
+// Node.random 为空（null）或指向链表中的节点。 
+// 节点数目不超过 1000 。 
+// 
+//
+// 
+//
+// 注意：本题与主站 138 题相同：https://leetcode-cn.com/problems/copy-list-with-random-point
+//er/ 
+//
+// 
+// Related Topics 链表 
+// 👍 146 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+class Solution {
+    // 深拷贝原来的链表，而不是只是复制其引用
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        // 创建原始链表和新链表节点之间的映射
+        HashMap<Node, Node> map = new HashMap<>();
+        Node cur = head;
+        // map 中 key 存放的是 head 中各个节点的引用，所以包含其 next 和 random
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        // 重来一遍来
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
