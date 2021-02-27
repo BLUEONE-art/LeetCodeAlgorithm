b612d8e0cd4b8eace72d0586152b74de9dce1ddf
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-02-27 10:24:53 +0800

    刷题

---
+++ 剑指Offer/leetcode/editor/cn/[剑指 Offer 49]丑数.java
@@ -0,0 +1,42 @@
+//我们把只包含质因子 2、3 和 5 的数称作丑数（Ugly Number）。求按从小到大的顺序的第 n 个丑数。 
+//
+// 
+//
+// 示例: 
+//
+// 输入: n = 10
+//输出: 12
+//解释: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 是前 10 个丑数。 
+//
+// 说明: 
+//
+// 
+// 1 是丑数。 
+// n 不超过1690。 
+// 
+//
+// 注意：本题与主站 264 题相同：https://leetcode-cn.com/problems/ugly-number-ii/ 
+// Related Topics 数学 
+// 👍 118 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int nthUglyNumber(int n) {
+        // dp[]：第 n 个丑数为 dp[n]
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int a = 0, b = 0, c = 0;
+        for (int i = 1; i < n; i++) {
+            int n1 = dp[a] * 2;
+            int n2 = dp[b] * 3;
+            int n3 = dp[c] * 5;
+            dp[i] = Math.min(Math.min(n1, n2), n3);
+            if (dp[i] == n1) a++;
+            if (dp[i] == n2) b++;
+            if (dp[i] == n3) c++;
+        }
+        return dp[n - 1];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
