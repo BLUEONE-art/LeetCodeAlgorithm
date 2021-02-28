953ee839d133ed598ae60ba76e212d2a77ef6c3f
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-02-28 23:16:09 +0800

    刷题笔记

---
+++ 剑指Offer/leetcode/editor/cn/[剑指 Offer 63]股票的最大利润.java
@@ -0,0 +1,54 @@
+//假设把某股票的价格按照时间先后顺序存储在数组中，请问买卖该股票一次可能获得的最大利润是多少？ 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: [7,1,5,3,6,4]
+//输出: 5
+//解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+//     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+// 
+//
+// 示例 2: 
+//
+// 输入: [7,6,4,3,1]
+//输出: 0
+//解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 数组长度 <= 10^5 
+//
+// 
+//
+// 注意：本题与主站 121 题相同：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-s
+//tock/ 
+// Related Topics 动态规划 
+// 👍 92 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxProfit(int[] prices) {
+        // dp[i]：以 prices[i] 结尾的子数组的最大利润 --> dp[n] = Max(dp[n - 1], prices[i] - min(price[0 : i]))
+        int cost = Integer.MAX_VALUE, profit = 0;
+        for (int price : prices) {
+            cost = Math.min(cost, price);
+            profit = Math.max(profit, price - cost);
+        }
+        return profit;
+
+        // 暴力解法
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                res = Math.max(res, prices[j] - prices[i]);
+            }
+        }
+        return res < 0 ? 0 : res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
