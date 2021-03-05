f27afcbdc258fcb2e0cd7602b1bec16c5eb61ee5
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-03-05 10:55:35 +0800

    刷题

--- DP/leetcode/editor/cn/[309]最佳买卖股票时机含冷冻期.java
+++ DP/leetcode/editor/cn/[309]最佳买卖股票时机含冷冻期.java
@@ -33,6 +33,7 @@ class Solution {
             int tmp = dp_i_0;
             dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
             dp_i_1 = Math.max(dp_i_1, dp_pre_0 - prices[i]);
+            // dp_pre_0 = dp[i - 2][0]，即下一次更新到 dp[i - 1][0]
             dp_pre_0 = tmp;
         }
         return dp_i_0;
