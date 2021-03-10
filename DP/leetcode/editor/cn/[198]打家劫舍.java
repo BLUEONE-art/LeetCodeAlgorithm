bc31e7b90a47d803e0a2f5bdd09d620b67b3dc4a
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-03-10 11:29:03 +0800

    刷题

--- DP/leetcode/editor/cn/[198]打家劫舍.java
+++ DP/leetcode/editor/cn/[198]打家劫舍.java
@@ -50,5 +50,19 @@ class Solution {
         }
         return dp[0];
     }
+
+    int rob(int[] nums) {
+        int n = nums.length;
+        // 记录 dp[i+1] 和 dp[i+2]
+        int dp_i_1 = 0, dp_i_2 = 0;
+        // 记录 dp[i]
+        int dp_i = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            dp_i = Math.max(dp_i_1, nums[i] + dp_i_2);
+            dp_i_2 = dp_i_1;
+            dp_i_1 = dp_i;
+        }
+        return dp_i;
+    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
