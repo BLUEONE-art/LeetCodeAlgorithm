154f7204b3f5698e93a80f968b715e5911692838
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-03-10 12:17:30 +0800

    刷题

---
+++ DP/leetcode/editor/cn/[213]打家劫舍 II.java
@@ -0,0 +1,74 @@
+//你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的
+//房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。 
+//
+// 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，能够偷窃到的最高金额。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [2,3,2]
+//输出：3
+//解释：你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1,2,3,1]
+//输出：4
+//解释：你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+//     偷窃到的最高金额 = 1 + 3 = 4 。 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [0]
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 100 
+// 0 <= nums[i] <= 1000 
+// 
+// Related Topics 动态规划 
+// 👍 484 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int rob(int[] nums) {
+        // 假如只有一间房子
+        int len = nums.length;
+        if (len == 1) return nums[0];
+        // 为了避免环形，比如 [2, 3, 2] 返回 3
+        return Math.max(
+                // 从 0 ~ n - 2
+                robRange(nums, 0, len - 2),
+                robRange(nums, 1, len - 1)
+        );
+    }
+    public int robRange(int[] nums, int start, int end) {
+        // base case
+        int dp_i_1 = 0, dp_i_2 = 0;
+        int dp_i = 0;
+        for (int i = end; i >= start; i--) {
+            dp_i = Math.max(
+                    // 抢了，下一家
+                    nums[i] + dp_i_2,
+                    // 没抢，下一家
+                    dp_i_1
+            );
+            // 更新
+            dp_i_2 = dp_i_1;
+            dp_i_1 = dp_i;
+        }
+        return dp_i;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
