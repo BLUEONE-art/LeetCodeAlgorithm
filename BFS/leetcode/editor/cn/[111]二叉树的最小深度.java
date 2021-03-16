346e8effd0c0520e457805b86e8d7f9d28897888
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-03-16 15:20:44 +0800

    刷题

---
+++ BFS/leetcode/editor/cn/[111]二叉树的最小深度.java
@@ -0,0 +1,74 @@
+//给定一个二叉树，找出其最小深度。 
+//
+// 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。 
+//
+// 说明：叶子节点是指没有子节点的节点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：2
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [2,null,3,null,4,null,5,null,6]
+//输出：5
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数的范围在 [0, 105] 内 
+// -1000 <= Node.val <= 1000 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 
+// 👍 472 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // 广度优先搜索
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        // 核心数据结构：队列
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        // 初始深度为 1
+        int depth = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                if (cur.left == null && cur.right == null) return depth;
+                // 放入每一层的节点
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
