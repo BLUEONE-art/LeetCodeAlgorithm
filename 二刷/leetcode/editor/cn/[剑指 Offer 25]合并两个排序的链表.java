3ad43bf0cf973f28fa7adbf8683f946158260e5f
Dong Hao <2191380@mail.dhu.edu.cn>
2021-03-26 23:37:26 +0800

    刷题

--- 二刷/leetcode/editor/cn/[剑指 Offer 25]合并两个排序的链表.java
+++ 二刷/leetcode/editor/cn/[剑指 Offer 25]合并两个排序的链表.java
@@ -25,20 +25,20 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode res = new ListNode(0), cur = res;
+        ListNode slow = new ListNode(0), fast = slow;
         while (l1 != null && l2 != null) {
             if (l1.val >= l2.val) {
-                cur.next = l2;
+                fast.next = l2;
                 l2 = l2.next;
             } else {
-                cur.next = l1;
+                fast.next = l1;
                 l1 = l1.next;
             }
-            cur = cur.next;
+            fast = fast.next;
         }
         // 哪个先跑完
-        cur.next = (l1 == null) ? l2 : l1;
-        return res.next;
+        fast.next = (l1 == null) ? l2 : l1;
+        return slow.next;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
