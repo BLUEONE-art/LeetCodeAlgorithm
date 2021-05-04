b2f39465bba486a93134593e36a4fe6f64c0356c
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-05-04 23:42:51 +0800

    刷题

---
+++ BFS/leetcode/editor/cn/[199]二叉树的右视图.java
@@ -0,0 +1,53 @@
+//给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。 
+//
+// 示例: 
+//
+// 输入: [1,2,3,null,5,null,4]
+//输出: [1, 3, 4]
+//解释:
+//
+//   1            <---
+// /   \
+//2     3         <---
+// \     \
+//  5     4       <---
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 递归 队列 
+// 👍 453 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = size - 1; i >= 0; i--) {
+                TreeNode cur = q.poll();
+                if (cur.left != null) q.offer(cur.left);
+                if (cur.right != null) q.offer(cur.right);
+                if (i == 0) res.add(cur.val);
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
