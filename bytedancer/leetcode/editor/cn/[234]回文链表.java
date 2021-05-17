ba292683edeca09dd9f55eff71d186a27f6ce0cd
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-05-17 14:50:39 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[234]回文链表.java
@@ -0,0 +1,66 @@
+//请判断一个链表是否为回文链表。 
+//
+// 示例 1: 
+//
+// 输入: 1->2
+//输出: false 
+//
+// 示例 2: 
+//
+// 输入: 1->2->2->1
+//输出: true
+// 
+//
+// 进阶： 
+//你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？ 
+// Related Topics 链表 双指针 
+// 👍 982 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) return false;
+        ListNode left = head;
+        ListNode mid = middleNode(head);
+        mid = reverse(mid);
+        while (mid != null) {
+            if (left.val != mid.val) return false;
+            left = left.next;
+            mid = mid.next;
+        }
+        return true;
+    }
+
+    public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return fast == null ? slow : slow.next;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode pre = null, cur = head, nxt = head;
+        while (cur != null) {
+            nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        return pre;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
