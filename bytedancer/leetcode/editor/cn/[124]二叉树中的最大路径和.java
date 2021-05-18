5256588d35a6a260875fc7a45f7fa6d27b268f11
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-05-18 16:18:55 +0800

    刷题

--- bytedancer/leetcode/editor/cn/[124]二叉树中的最大路径和.java
+++ bytedancer/leetcode/editor/cn/[124]二叉树中的最大路径和.java
@@ -51,8 +51,19 @@
  * }
  */
 class Solution {
+    int max = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return max;
+    }
 
+    // 返回包括root节点的单边路径最大值
+    public int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int left = Math.max(0, dfs(root.left));
+        int right = Math.max(0, dfs(root.right));
+        max = Math.max(max, root.val + left + right);
+        return root.val + Math.max(left, right); // 单边最大路径
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
