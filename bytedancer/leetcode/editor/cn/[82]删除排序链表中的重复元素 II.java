61dfd24a974bdab8703c662b0bfb0eebfba20ae2
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-05-18 19:21:17 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[82]删除排序链表中的重复元素 II.java
@@ -0,0 +1,61 @@
+//存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除链表中所有存在数字重复情况的节点，只保留原始链表中 没有重复出现 的数字。 
+//
+// 返回同样按升序排列的结果链表。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,3,4,4,5]
+//输出：[1,2,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1,1,1,2,3]
+//输出：[2,3]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数目在范围 [0, 300] 内 
+// -100 <= Node.val <= 100 
+// 题目数据保证链表已经按升序排列 
+// 
+// Related Topics 链表 
+// 👍 615 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode dum = new ListNode(), cur = dum;
+        while (head != null) {
+            if (head.next == null || head.val != head.next.val) {
+                cur.next = head;
+                cur = cur.next;
+            }
+            while (head.next != null && head.val == head.next.val) head = head.next;
+            head = head.next;
+        }
+        cur.next = null;
+        return dum.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
