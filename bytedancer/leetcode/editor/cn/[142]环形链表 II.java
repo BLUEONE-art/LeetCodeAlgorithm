5ff105da57fc85de4b6b7790a07512c878f4d447
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-05-18 13:49:58 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[142]环形链表 II.java
@@ -0,0 +1,90 @@
+//给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。 
+//
+// 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意，po
+//s 仅仅是用于标识环的情况，并不会作为参数传递到函数中。 
+//
+// 说明：不允许修改给定的链表。 
+//
+// 进阶： 
+//
+// 
+// 你是否可以使用 O(1) 空间解决此题？ 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：head = [3,2,0,-4], pos = 1
+//输出：返回索引为 1 的链表节点
+//解释：链表中有一个环，其尾部连接到第二个节点。
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：head = [1,2], pos = 0
+//输出：返回索引为 0 的链表节点
+//解释：链表中有一个环，其尾部连接到第一个节点。
+// 
+//
+// 示例 3： 
+//
+// 
+//
+// 
+//输入：head = [1], pos = -1
+//输出：返回 null
+//解释：链表中没有环。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目范围在范围 [0, 104] 内 
+// -105 <= Node.val <= 105 
+// pos 的值为 -1 或者链表中的一个有效索引 
+// 
+// Related Topics 链表 双指针 
+// 👍 997 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (true) {
+            if (fast == null || fast.next == null) return null;
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) break;
+        }
+        // 设slow走了s步，fast是slow的两倍，所以f = 2s；并且在环中相遇时，f = s + nb(b：环中的节点数)
+        // f = 2nb，s = nb；只要都再走a(head到环入口的节点数)步，即在环入口相遇
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
