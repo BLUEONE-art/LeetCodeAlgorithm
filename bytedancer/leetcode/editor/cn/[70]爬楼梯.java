f59d86ae136f1b0e3ad55eb00e5cf2c7b6eec850
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-05-19 22:53:16 +0800

    刷题

--- bytedancer/leetcode/editor/cn/[70]爬楼梯.java
+++ bytedancer/leetcode/editor/cn/[70]爬楼梯.java
@@ -30,13 +30,20 @@
 //        return climbStairs(n - 1) + climbStairs(n - 2);
 //    }
 
+//        int[] dp = new int[n + 1];
+//        dp[0] = dp[1] = 1;
+//        for (int i = 2; i <= n; i++) {
+//            dp[i] = dp[i - 1] + dp[i -2];
+//        }
+//        return dp[n];
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n + 1];
-        dp[0] = dp[1] = 1;
+        dp[0] = 1; dp[1] = 1;
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i -2];
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
     }
