3e7842e9ad937ec93898b71ee32d632b5b6f0990
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-05-21 11:36:55 +0800

    刷题

--- bytedancer/leetcode/editor/cn/[912]排序数组.java
+++ bytedancer/leetcode/editor/cn/[912]排序数组.java
@@ -26,21 +26,50 @@
 // -50000 <= nums[i] <= 50000
 // 👍 286 👎 0
 
+//         if (nums.length <= 1) return nums;
+//        // 大顶堆
+//        Queue<Integer> q = new PriorityQueue<>((a, b) -> b - a);
+//        for (int num : nums) {
+//            q.offer(num);
+//        }
+//        int[] res = new int[nums.length];
+//        for (int i = nums.length - 1; i >= 0; i--) {
+//            res[i] = q.poll();
+//        }
+//        return res;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] sortArray(int[] nums) {
-        if (nums.length <= 1) return nums;
-        // 大顶堆
-        Queue<Integer> q = new PriorityQueue<>((a, b) -> b - a);
-        for (int num : nums) {
-            q.offer(num);
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    public void quickSort(int[] nums, int left, int right) {
+        if (left >= right) return;
+        int j = partition(nums, left, right);
+        quickSort(nums, left, j - 1);
+        quickSort(nums, j + 1, right);
+    }
+
+    public int partition(int[] nums, int left, int right) {
+        int leftMostNum = nums[left];
+        int i = left, j = right + 1;
+        while (true) {
+            while (++i < right && nums[i] < leftMostNum);
+            while (--j > left && nums[j] > leftMostNum);
+            if (i >= j) break;
+            swap(nums, i, j);
         }
-        int[] res = new int[nums.length];
-        for (int i = nums.length - 1; i >= 0; i--) {
-            res[i] = q.poll();
-        }
-        return res;
+        nums[left] = nums[j];
+        nums[j] = leftMostNum;
+        return j;
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
