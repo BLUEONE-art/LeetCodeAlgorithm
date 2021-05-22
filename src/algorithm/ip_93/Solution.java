14537a5f83eeb1b2fdd9f18605d714e61e5bf606
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-05-22 20:47:50 +0800

    刷题

--- src/algorithm/ip_93/Solution.java
+++ src/algorithm/ip_93/Solution.java
@@ -7,27 +7,25 @@ import java.util.Stack;
 public class Solution {
     public static void main(String[] args) {
         int[] a = new int[]{2,3,1,2,4,3};
-        System.out.println(minSubArrayLen(7, a));
+        System.out.println(longestCommonSubsequence("abcde", "ace"));
     }
 
-    public static int minSubArrayLen(int target, int[] nums) {
-        int left = 0, right = 0, start = 0, len = Integer.MAX_VALUE;
-        while (right < nums.length) {
-            int sum = 0;
-            sum += nums[right];
-            right++;
-            while (sum >= target) { // 开始收缩窗口
-                // 更新len
-                if (right - left < len) {
-                    start = left;
-                    len = Math.min(len, right - left);
+    public static int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        int res = 0;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 }
-                int tmp = nums[left];
-                sum -= tmp;
-                left++;
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+                res = Math.max(res, dp[i][j]);
             }
         }
-        return len;
+        return res;
     }
 }
 
