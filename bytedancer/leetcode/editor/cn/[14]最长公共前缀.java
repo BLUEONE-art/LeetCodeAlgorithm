1ffba524f3e7be783a85f527a3be0d02cf2fb351
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-06-01 08:20:36 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[14]最长公共前缀.java
@@ -0,0 +1,51 @@
+//编写一个函数来查找字符串数组中的最长公共前缀。 
+//
+// 如果不存在公共前缀，返回空字符串 ""。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：strs = ["flower","flow","flight"]
+//输出："fl"
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：strs = ["dog","racecar","car"]
+//输出：""
+//解释：输入不存在公共前缀。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= strs.length <= 200 
+// 0 <= strs[i].length <= 200 
+// strs[i] 仅由小写英文字母组成 
+// 
+// Related Topics 字符串 
+// 👍 1613 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String ans = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            int j = 0;
+            for (; j < strs[i].length() && j < ans.length(); j++) {
+                if (strs[i].charAt(j) != ans.charAt(j)) break;
+            }
+            ans = ans.substring(0, j);
+            // 如果出现ans == ""，直接返回
+            if (ans.equals("")) return ans;
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
