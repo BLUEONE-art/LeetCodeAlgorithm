c7158256e0fca1dd290c45b84b85eb1429549b6f
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-06-13 17:21:51 +0800

    刷题

--- bytedancer/leetcode/editor/cn/[1143]最长公共子序列.java
+++ bytedancer/leetcode/editor/cn/[1143]最长公共子序列.java
@@ -49,21 +49,19 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length(), n = text2.length();
-        int res = 0;
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
+        int len1 = text1.length(), len2 = text2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 }
                 else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
-                res = Math.max(res, dp[i][j]);
             }
         }
-        return res;
+        return dp[len1][len2];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
