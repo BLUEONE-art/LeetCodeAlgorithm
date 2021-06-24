831bb6c544735810c93924cf7e0febd08954aaa9
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-06-24 22:40:20 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[剑指 Offer 53 - I]在排序数组中查找数字 I.java
@@ -0,0 +1,52 @@
+//统计一个数字在排序数组中出现的次数。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: nums = [5,7,7,8,8,10], target = 8
+//输出: 2 
+//
+// 示例 2: 
+//
+// 输入: nums = [5,7,7,8,8,10], target = 6
+//输出: 0 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 数组长度 <= 50000 
+//
+// 
+//
+// 注意：本题与主站 34 题相同（仅返回值不同）：https://leetcode-cn.com/problems/find-first-and-last-
+//position-of-element-in-sorted-array/ 
+// Related Topics 数组 二分查找 
+// 👍 145 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int search(int[] nums, int target) {
+        return helper(nums, target) - helper(nums, target - 1);
+    }
+
+    public int helper(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                left = mid + 1;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return right - 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
