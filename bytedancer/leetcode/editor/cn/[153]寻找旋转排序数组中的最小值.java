8393906e96e1e49724464a36c00d3725fb7c759c
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-06-25 17:03:19 +0800

    刷题

--- bytedancer/leetcode/editor/cn/[153]寻找旋转排序数组中的最小值.java
+++ bytedancer/leetcode/editor/cn/[153]寻找旋转排序数组中的最小值.java
@@ -57,14 +57,13 @@ class Solution {
         int left = 0, right = nums.length - 1;
         while (left < right) {
             int mid = (left + right) / 2;
-            if (nums[mid] <= nums[right]) {
-                right = mid;
-            }
-            else {
+            if (nums[mid] >= nums[right]) {
                 left = mid + 1;
-            }
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else break;
         }
-        return nums[right];
+        return nums[left] < nums[right] ? nums[left] : nums[right];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
