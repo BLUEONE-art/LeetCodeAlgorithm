9c229cddf5ba938f1893a56ff56fcc2d63bbdcdc
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-06-25 14:27:50 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[54]螺旋矩阵.java
@@ -0,0 +1,70 @@
+//给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+//输出：[1,2,3,6,9,8,7,4,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+//输出：[1,2,3,4,8,12,11,10,9,5,6,7]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == matrix.length 
+// n == matrix[i].length 
+// 1 <= m, n <= 10 
+// -100 <= matrix[i][j] <= 100 
+// 
+// Related Topics 数组 矩阵 模拟 
+// 👍 808 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int top = 0, bottom = matrix.length - 1, left = 0, right = matrix[0].length - 1;
+        while (true) {
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            if (++top > bottom) {
+                break;
+            }
+
+            for (int j = top; j <= bottom; j++) {
+                res.add(matrix[j][right]);
+            }
+            if (--right < left) {
+                break;
+            }
+
+            for (int k = right; k >= left; k--) {
+                res.add(matrix[bottom][k]);
+            }
+            if (--bottom < top) {
+                break;
+            }
+
+            for (int l = bottom; l >= top; l--) {
+                res.add(matrix[l][left]);
+            }
+            if (++left > right) {
+                break;
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
