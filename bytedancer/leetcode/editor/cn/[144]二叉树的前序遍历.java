73785f9a965aa14c1e8e34add5569e52915075a8
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-07-01 18:00:24 +0800

    刷题

--- bytedancer/leetcode/editor/cn/[144]二叉树的前序遍历.java
+++ bytedancer/leetcode/editor/cn/[144]二叉树的前序遍历.java
@@ -70,14 +70,33 @@
  * }
  */
 class Solution {
-    List<Integer> res = new ArrayList<>();
+//    public List<Integer> preorderTraversal(TreeNode root) {
+//        if (root == null) {
+//            return new ArrayList<>();
+//        }
+//        res.add(root.val);
+//        preorderTraversal(root.left);
+//        preorderTraversal(root.right);
+//        return res;
+//    }
+
     public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
         if (root == null) {
             return new ArrayList<>();
         }
-        res.add(root.val);
-        preorderTraversal(root.left);
-        preorderTraversal(root.right);
+        Stack<TreeNode> q = new Stack<>();
+        q.push(root);
+        while (!q.isEmpty()) {
+            TreeNode cur = q.pop();
+            res.add(cur.val);
+            if (cur.right != null) {
+                q.push(cur.right);
+            }
+            if (cur.left != null) {
+                q.push(cur.left);
+            }
+        }
         return res;
     }
 }
