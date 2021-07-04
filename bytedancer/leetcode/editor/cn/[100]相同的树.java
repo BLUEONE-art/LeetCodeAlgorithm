cc2f951897d980f4901687b4d75f015cd87801dd
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-07-04 22:00:47 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[100]相同的树.java
@@ -0,0 +1,84 @@
+//给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。 
+//
+// 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：p = [1,2,3], q = [1,2,3]
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：p = [1,2], q = [1,null,2]
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：p = [1,2,1], q = [1,1,2]
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 两棵树上的节点数目都在范围 [0, 100] 内 
+// -104 <= Node.val <= 104 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 
+// 👍 646 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        TreeNode root1 = p, root2 = q;
+        inorder(root1, sb1);
+        inorder(root2, sb2);
+        char[] chars1 = sb1.toString().toCharArray();
+        char[] chars2 = sb2.toString().toCharArray();
+        if (chars1.length != chars2.length) return false;
+        for (int i = 0; i < chars1.length; i++) {
+            if (chars1[i] != chars2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void inorder(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append('#');
+            return;
+        }
+        sb.append(root.val);
+        inorder(root.left, sb);
+        inorder(root.right, sb);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
