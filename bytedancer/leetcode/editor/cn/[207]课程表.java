508561f0e59cdcf37b49232a3acd1b7747b4415d
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-08-01 23:26:13 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[207]课程表.java
@@ -0,0 +1,87 @@
+//你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。 
+//
+// 在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，其中 prerequisites[i] = [ai, bi] ，表
+//示如果要学习课程 ai 则 必须 先学习课程 bi 。 
+//
+// 
+// 例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。 
+// 
+//
+// 请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：numCourses = 2, prerequisites = [[1,0]]
+//输出：true
+//解释：总共有 2 门课程。学习课程 1 之前，你需要完成课程 0 。这是可能的。 
+//
+// 示例 2： 
+//
+// 
+//输入：numCourses = 2, prerequisites = [[1,0],[0,1]]
+//输出：false
+//解释：总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= numCourses <= 105 
+// 0 <= prerequisites.length <= 5000 
+// prerequisites[i].length == 2 
+// 0 <= ai, bi < numCourses 
+// prerequisites[i] 中的所有课程对 互不相同 
+// 
+// Related Topics 深度优先搜索 广度优先搜索 图 拓扑排序 
+// 👍 884 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        boolean flag = true;
+        int[] inDegree = new int[numCourses];
+        List<List<Integer>> adjLists = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adjLists.add(new ArrayList<>());
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            int cur = prerequisites[i][1];
+            int next = prerequisites[i][0];
+            inDegree[next]++;
+            adjLists.get(cur).add(next);
+        }
+        for (int i = 0; i < inDegree.length; i++) {
+            if (inDegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int cur = q.poll();
+                res.add(cur);
+                List<Integer> adjList = adjLists.get(cur);
+                for (Integer adj : adjList) {
+                    inDegree[adj]--;
+                    if (inDegree[adj] == 0) {
+                        q.offer(adj);
+                    }
+                }
+            }
+        }
+        if (res.size() == numCourses) {
+            flag = flag;
+        } else {
+            flag = !flag;
+        }
+        return flag;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
