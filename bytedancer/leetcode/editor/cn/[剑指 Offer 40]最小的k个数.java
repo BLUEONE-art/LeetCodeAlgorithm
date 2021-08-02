2490f1178d63300fc6097df49e29368dc8c82155
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-08-02 22:08:18 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[剑指 Offer 40]最小的k个数.java
@@ -0,0 +1,43 @@
+//输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：arr = [3,2,1], k = 2
+//输出：[1,2] 或者 [2,1]
+// 
+//
+// 示例 2： 
+//
+// 输入：arr = [0,1,2,1], k = 1
+//输出：[0] 
+//
+// 
+//
+// 限制： 
+//
+// 
+// 0 <= k <= arr.length <= 10000 
+// 0 <= arr[i] <= 10000 
+// 
+// Related Topics 数组 分治 快速选择 排序 堆（优先队列） 
+// 👍 273 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] getLeastNumbers(int[] arr, int k) {
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        List<Integer> res = new ArrayList<>();
+        int idx = 0;
+        for (int i : arr) {
+            q.offer(i);
+            if (q.size() > arr.length - k) {
+                res.add(q.poll());
+            }
+        }
+        return res.stream().mapToInt(Integer::valueOf).toArray();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
