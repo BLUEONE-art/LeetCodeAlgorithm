8d7c94adb4b53cf17d74c04f24805e504bf2d33c
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-08-04 23:07:00 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[86]分隔链表.java
@@ -0,0 +1,66 @@
+//给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。 
+//
+// 你应当 保留 两个分区中每个节点的初始相对位置。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,4,3,2,5,2], x = 3
+//输出：[1,2,2,4,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [2,1], x = 2
+//输出：[1,2]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 200] 内 
+// -100 <= Node.val <= 100 
+// -200 <= x <= 200 
+// 
+// Related Topics 链表 双指针 
+// 👍 434 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode smallHead = new ListNode(0);
+        ListNode smallTail = smallHead;
+        ListNode bigHead = new ListNode(0);
+        ListNode bigTail = bigHead;
+        while (head != null) {
+            if (head.val < x) {
+                smallTail.next = head;
+                smallTail = smallTail.next;
+            } else {
+                bigTail.next = head;
+                bigTail = bigTail.next;
+            }
+            head = head.next;
+        }
+        smallTail.next = bigHead.next;
+        bigTail.next = null;
+        return smallHead.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
