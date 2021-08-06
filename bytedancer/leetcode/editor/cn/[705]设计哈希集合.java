7d1281ae2eea0b95822e5f4a8d4831ff7255ede6
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-08-06 15:52:07 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[705]设计哈希集合.java
@@ -0,0 +1,78 @@
+//不使用任何内建的哈希表库设计一个哈希集合（HashSet）。 
+//
+// 实现 MyHashSet 类： 
+//
+// 
+// void add(key) 向哈希集合中插入值 key 。 
+// bool contains(key) 返回哈希集合中是否存在这个值 key 。 
+// void remove(key) 将给定值 key 从哈希集合中删除。如果哈希集合中没有这个值，什么也不做。 
+// 
+// 
+//
+// 示例： 
+//
+// 
+//输入：
+//["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove
+//", "contains"]
+//[[], [1], [2], [1], [3], [2], [2], [2], [2]]
+//输出：
+//[null, null, null, true, false, null, true, null, false]
+//
+//解释：
+//MyHashSet myHashSet = new MyHashSet();
+//myHashSet.add(1);      // set = [1]
+//myHashSet.add(2);      // set = [1, 2]
+//myHashSet.contains(1); // 返回 True
+//myHashSet.contains(3); // 返回 False ，（未找到）
+//myHashSet.add(2);      // set = [1, 2]
+//myHashSet.contains(2); // 返回 True
+//myHashSet.remove(2);   // set = [1]
+//myHashSet.contains(2); // 返回 False ，（已移除） 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= key <= 106 
+// 最多调用 104 次 add、remove 和 contains 。 
+// 
+//
+// 
+//
+// 进阶：你可以不使用内建的哈希集合库解决此问题吗？ 
+// Related Topics 设计 数组 哈希表 链表 哈希函数 
+// 👍 171 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class MyHashSet {
+    HashSet<Integer> set;
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        set = new HashSet<>();
+    }
+    
+    public void add(int key) {
+        set.add(key);
+    }
+    
+    public void remove(int key) {
+        set.remove(key);
+    }
+    
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return set.contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
