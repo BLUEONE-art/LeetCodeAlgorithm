80a8dd15ee0fed2a1ab2d2d775b6b133b51c8118
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-08-08 11:38:56 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[77]组合.java
@@ -0,0 +1,63 @@
+//给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。 
+//
+// 你可以按 任何顺序 返回答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 4, k = 2
+//输出：
+//[
+//  [2,4],
+//  [3,4],
+//  [2,3],
+//  [1,2],
+//  [1,3],
+//  [1,4],
+//] 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 1, k = 1
+//输出：[[1]] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 20 
+// 1 <= k <= n 
+// 
+// Related Topics 数组 回溯 
+// 👍 648 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<Integer> path = new ArrayDeque<>();
+        backtrack(n, k, 1, path, res);
+        return res;
+    }
+
+    public void backtrack(int n, int k, int depth, Deque<Integer> path, List<List<Integer>> res) {
+        if (path.size() == k) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = depth; i <= n; i++) {
+            // 做选择
+            path.addLast(i);
+            // 回溯框架
+            backtrack(n, k, i + 1, path, res);
+            // 撤销选择
+            path.removeLast();
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
