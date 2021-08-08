ab3a048455ca047e023cf7b52e78df184bab04d1
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-08-08 21:41:25 +0800

    刷题

--- bytedancer/leetcode/editor/cn/[153]寻找旋转排序数组中的最小值.java
+++ bytedancer/leetcode/editor/cn/[153]寻找旋转排序数组中的最小值.java
@@ -56,7 +56,7 @@ class Solution {
     public int findMin(int[] nums) {
         int left = 0, right = nums.length - 1;
         while (left < right) {
-            int mid = (left + right) / 2;
+            int mid = left + ((right - left) >> 1);
             if (nums[mid] >= nums[right]) {
                 left = mid + 1;
             } else if (nums[mid] < nums[right]) {
