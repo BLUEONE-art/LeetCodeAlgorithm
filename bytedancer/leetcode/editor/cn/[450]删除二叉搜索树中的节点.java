be493da6f88a82b67ec29ae61eecbd830256f02f
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-08-08 09:18:44 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[450]删除二叉搜索树中的节点.java
@@ -0,0 +1,96 @@
+//给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key 对应的节点，并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的
+//根节点的引用。 
+//
+// 一般来说，删除节点可分为两个步骤： 
+//
+// 
+// 首先找到需要删除的节点； 
+// 如果找到了，删除它。 
+// 
+//
+// 说明： 要求算法时间复杂度为 O(h)，h 为树的高度。 
+//
+// 示例: 
+//
+// 
+//root = [5,3,6,2,4,null,7]
+//key = 3
+//
+//    5
+//   / \
+//  3   6
+// / \   \
+//2   4   7
+//
+//给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+//
+//一个正确的答案是 [5,4,6,2,null,null,7], 如下图所示。
+//
+//    5
+//   / \
+//  4   6
+// /     \
+//2       7
+//
+//另一个正确答案是 [5,2,6,null,4,null,7]。
+//
+//    5
+//   / \
+//  2   6
+//   \   \
+//    4   7
+// 
+// Related Topics 树 二叉搜索树 二叉树 
+// 👍 493 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (key == root.val) {
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            if (root.left == null || root.right == null) {
+                return root.left == null ? root.right : root.left;
+            }
+            // 两个子节点均不为null
+            TreeNode rightMinNode = getMin(root.right);
+            root.val = rightMinNode.val;
+            root.right = deleteNode(root.right, rightMinNode.val);
+        }
+        if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        }
+        if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        }
+        return root;
+    }
+
+    public TreeNode getMin(TreeNode root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
