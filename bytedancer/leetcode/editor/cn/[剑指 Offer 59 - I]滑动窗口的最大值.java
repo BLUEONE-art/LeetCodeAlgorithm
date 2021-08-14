13cc7a4f9d0c4434052185e2f96bcbda6dc9f13c
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-08-14 16:20:29 +0800

    刷题

--- bytedancer/leetcode/editor/cn/[剑指 Offer 59 - I]滑动窗口的最大值.java
+++ bytedancer/leetcode/editor/cn/[剑指 Offer 59 - I]滑动窗口的最大值.java
@@ -22,60 +22,64 @@
 // 你可以假设 k 总是有效的，在输入数组不为空的情况下，1 ≤ k ≤ 输入数组的大小。 
 //
 // 注意：本题与主站 239 题相同：https://leetcode-cn.com/problems/sliding-window-maximum/ 
-// Related Topics 队列 Sliding Window 
-// 👍 187 👎 0
+// Related Topics 队列 滑动窗口 单调队列 堆（优先队列） 
+// 👍 310 👎 0
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    // 单调递减队列
     public class MonotonicQueue {
-        // 双向链表实现单调队列，有入队、出队、求最大值三种 API
-        LinkedList<Integer> q = new LinkedList<>();
+        LinkedList<Integer> q;
 
-        // 入队时保证队列单调递减的特性
-        public void push(int n) {
-            // 进来先判断：只有队列的末尾元素比要添加元素大或等于，才能添加入队
-            while (!q.isEmpty() && q.getLast() < n) {
-                q.pollLast();
-            }
-            q.addLast(n);
+        /**
+         * 初始化
+         */
+        public MonotonicQueue() {
+            q = new LinkedList<>();
         }
 
-        // 求队列中的最大值，即为队头元素
+        /**
+         * 添加元素
+         */
+        public void push(int val) {
+            while (!q.isEmpty() && q.getLast() < val) {
+                q.pollLast();
+            }
+            q.addLast(val);
+        }
+
+        /**
+         * 获取队列最大值
+         * @return
+         */
         public int max() {
-            // 不用 pollFirst()：因为删除队列元素由 pop 完成
             return q.getFirst();
         }
 
-        // 出队
-        public void pop(int n) {
-            if (n == q.getFirst()) {
+        /**
+         * 删除元素
+         */
+        public void pop(int val) {
+            if (q.getFirst() == val) {
                 q.pollFirst();
             }
         }
     }
 
     public int[] maxSlidingWindow(int[] nums, int k) {
-        // 定义窗口
+        List<Integer> res = new ArrayList();
         MonotonicQueue window = new MonotonicQueue();
-        ArrayList<Integer> tmp = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
-            // 先装 k - 1 个
             if (i < k - 1) {
                 window.push(nums[i]);
             } else {
-                // 开始添加第 k 个元素
                 window.push(nums[i]);
-                tmp.add(window.max());
-                // 去除第一个元素，为下一次滑动窗口做准备
-                window.pop(nums[i - k + 1]);
+                res.add(window.max());
+                window.pop(nums[i + 1 - k]);
             }
         }
-        int[] res = new int[tmp.size()];
-        for (int i = 0; i < tmp.size(); i++) {
-            res[i] = tmp.get(i);
-        }
-        return res;
+        return res.stream().mapToInt(Integer::valueOf).toArray();
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
