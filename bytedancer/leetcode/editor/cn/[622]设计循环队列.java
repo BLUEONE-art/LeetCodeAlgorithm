9178b402f81f478541162e427732d57412803f55
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-08-14 15:17:06 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[622]设计循环队列.java
@@ -0,0 +1,115 @@
+//设计你的循环队列实现。 循环队列是一种线性数据结构，其操作表现基于 FIFO（先进先出）原则并且队尾被连接在队首之后以形成一个循环。它也被称为“环形缓冲器”
+//。 
+//
+// 循环队列的一个好处是我们可以利用这个队列之前用过的空间。在一个普通队列里，一旦一个队列满了，我们就不能插入下一个元素，即使在队列前面仍有空间。但是使用循环
+//队列，我们能使用这些空间去存储新的值。 
+//
+// 你的实现应该支持如下操作： 
+//
+// 
+// MyCircularQueue(k): 构造器，设置队列长度为 k 。 
+// Front: 从队首获取元素。如果队列为空，返回 -1 。 
+// Rear: 获取队尾元素。如果队列为空，返回 -1 。 
+// enQueue(value): 向循环队列插入一个元素。如果成功插入则返回真。 
+// deQueue(): 从循环队列中删除一个元素。如果成功删除则返回真。 
+// isEmpty(): 检查循环队列是否为空。 
+// isFull(): 检查循环队列是否已满。 
+// 
+//
+// 
+//
+// 示例： 
+//
+// MyCircularQueue circularQueue = new MyCircularQueue(3); // 设置长度为 3
+//circularQueue.enQueue(1);  // 返回 true
+//circularQueue.enQueue(2);  // 返回 true
+//circularQueue.enQueue(3);  // 返回 true
+//circularQueue.enQueue(4);  // 返回 false，队列已满
+//circularQueue.Rear();  // 返回 3
+//circularQueue.isFull();  // 返回 true
+//circularQueue.deQueue();  // 返回 true
+//circularQueue.enQueue(4);  // 返回 true
+//circularQueue.Rear();  // 返回 4 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 所有的值都在 0 至 1000 的范围内； 
+// 操作数将在 1 至 1000 的范围内； 
+// 请不要使用内置的队列库。 
+// 
+// Related Topics 设计 队列 数组 链表 
+// 👍 202 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class MyCircularQueue {
+    int[] queue;
+    // 当前队首元素下标
+    int front;
+    // 对首元素的下一个元素下标
+    int rear;
+    int cap;
+    public MyCircularQueue(int k) {
+        this.cap = k + 1;
+        this.queue = new int[cap];
+        this.front = 0;
+        this.rear = 0;
+    }
+
+    public boolean enQueue(int value) {
+        if (isFull()) {
+            return false;
+        }
+        queue[rear] = value;
+        rear = (rear + 1) % cap;
+        return true;
+    }
+    
+    public boolean deQueue() {
+        if (isEmpty()) {
+            return false;
+        }
+        // 当前位置元素会被后面循环的元素覆盖，直接更新front坐标即可
+        front = (front + 1) % cap;
+        return true;
+    }
+    
+    public int Front() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return queue[front];
+    }
+    
+    public int Rear() {
+        if (isEmpty()) {
+            return - 1;
+        }
+        // rear指向下一个入队位置，此位置没值，需要它上一个位置的值
+        return queue[(rear - 1 + cap) % cap];
+    }
+    
+    public boolean isEmpty() {
+        return front == rear;
+    }
+    
+    public boolean isFull() {
+        // 牺牲一个位置保证判空和判满条件不冲突
+        return (rear + 1) % cap == front;
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
