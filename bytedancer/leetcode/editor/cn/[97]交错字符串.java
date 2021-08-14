9edc7c0e11b72a62aeb9c0e72007bafae6968091
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-08-14 17:40:42 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[97]交错字符串.java
@@ -0,0 +1,77 @@
+//给定三个字符串 s1、s2、s3，请你帮忙验证 s3 是否是由 s1 和 s2 交错 组成的。 
+//
+// 两个字符串 s 和 t 交错 的定义与过程如下，其中每个字符串都会被分割成若干 非空 子字符串： 
+//
+// 
+// s = s1 + s2 + ... + sn 
+// t = t1 + t2 + ... + tm 
+// |n - m| <= 1 
+// 交错 是 s1 + t1 + s2 + t2 + s3 + t3 + ... 或者 t1 + s1 + t2 + s2 + t3 + s3 + ... 
+// 
+//
+// 提示：a + b 意味着字符串 a 和 b 连接。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s1 = "", s2 = "", s3 = ""
+//输出：true
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= s1.length, s2.length <= 100 
+// 0 <= s3.length <= 200 
+// s1、s2、和 s3 都由小写英文字母组成 
+// 
+// Related Topics 字符串 动态规划 
+// 👍 513 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int s1Len = s1.length();
+        int s2Len = s2.length();
+        int s3Len = s3.length();
+        if (s3Len != s1Len + s2Len) {
+            return false;
+        }
+        // dp[i][j] 表示s1的前i个字符能否与s2的前j个字符组成s3的前i+j个字符
+        boolean[][] dp = new boolean[s1Len + 1][s2Len + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= s1Len && s1.charAt(i - 1) == s3.charAt(i - 1); i++) {
+            dp[i][0] = true;
+        }
+        for (int j = 1; j <= s2Len && s2.charAt(j - 1) == s3.charAt(j - 1); j++) {
+            dp[0][j] = true;
+        }
+        for (int i = 1; i <= s1Len; i++) {
+            for (int j = 1; j <= s2Len; j++) {
+                dp[i][j] = (dp[i - 1][j] && s3.charAt(i + j - 1) == s1.charAt(i - 1))
+                        || (dp[i][j - 1] && s3.charAt(i + j - 1) == s2.charAt(j - 1));
+            }
+        }
+        return dp[s1Len][s2Len];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
