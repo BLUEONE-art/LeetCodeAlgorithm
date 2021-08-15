1bd9d29d3ed08bcc53851a1fdaeb9cea5431481a
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-08-15 14:59:40 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[540]有序数组中的单一元素.java
@@ -0,0 +1,47 @@
+//给定一个只包含整数的有序数组，每个元素都会出现两次，唯有一个数只会出现一次，找出这个数。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: nums = [1,1,2,3,3,4,4,8,8]
+//输出: 2
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: nums =  [3,3,7,7,10,11,11]
+//输出: 10
+// 
+//
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= nums.length <= 105 
+// 0 <= nums[i] <= 105 
+// 
+//
+// 
+//
+// 进阶: 采用的方案可以在 O(log n) 时间复杂度和 O(1) 空间复杂度中运行吗？ 
+// Related Topics 数组 二分查找 
+// 👍 256 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int ans = 0;
+        for (int num : nums) {
+            ans ^= num;
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
