b34a34eb74677861d820a0d4a5e57d1dcf27bac8
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-08-15 15:08:14 +0800

    刷题

--- bytedancer/leetcode/editor/cn/[540]有序数组中的单一元素.java
+++ bytedancer/leetcode/editor/cn/[540]有序数组中的单一元素.java
@@ -37,6 +37,24 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            // 出现两次的数下标都是偶数
+            if ((mid & 1) == 1) {
+                mid--;
+            }
+            if (nums[mid] == nums[mid + 1]) {
+                left = mid + 2;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+
+    public int singleNonDuplicate(int[] nums) {
         int ans = 0;
         for (int num : nums) {
             ans ^= num;
