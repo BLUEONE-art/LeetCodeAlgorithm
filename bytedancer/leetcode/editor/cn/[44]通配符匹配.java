4ea8731eb1eda5e428d25458a4f05f11dfbd1bbf
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-08-15 09:53:32 +0800

    刷题

---
+++ bytedancer/leetcode/editor/cn/[44]通配符匹配.java
@@ -0,0 +1,89 @@
+//给定一个字符串 (s) 和一个字符模式 (p) ，实现一个支持 '?' 和 '*' 的通配符匹配。 
+//
+// '?' 可以匹配任何单个字符。
+//'*' 可以匹配任意字符串（包括空字符串）。
+// 
+//
+// 两个字符串完全匹配才算匹配成功。 
+//
+// 说明: 
+//
+// 
+// s 可能为空，且只包含从 a-z 的小写字母。 
+// p 可能为空，且只包含从 a-z 的小写字母，以及字符 ? 和 *。 
+// 
+//
+// 示例 1: 
+//
+// 输入:
+//s = "aa"
+//p = "a"
+//输出: false
+//解释: "a" 无法匹配 "aa" 整个字符串。 
+//
+// 示例 2: 
+//
+// 输入:
+//s = "aa"
+//p = "*"
+//输出: true
+//解释: '*' 可以匹配任意字符串。
+// 
+//
+// 示例 3: 
+//
+// 输入:
+//s = "cb"
+//p = "?a"
+//输出: false
+//解释: '?' 可以匹配 'c', 但第二个 'a' 无法匹配 'b'。
+// 
+//
+// 示例 4: 
+//
+// 输入:
+//s = "adceb"
+//p = "*a*b"
+//输出: true
+//解释: 第一个 '*' 可以匹配空字符串, 第二个 '*' 可以匹配字符串 "dce".
+// 
+//
+// 示例 5: 
+//
+// 输入:
+//s = "acdcb"
+//p = "a*c?b"
+//输出: false 
+// Related Topics 贪心 递归 字符串 动态规划 
+// 👍 731 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    // s前i个字符是否和p前j个字符匹配
+    public boolean isMatch(String s, String p) {
+        int sLen = s.length();
+        int pLen = p.length();
+        boolean[][] dp = new boolean[sLen + 1][pLen + 1];
+        dp[0][0] = true;
+        // 当p全为*的情况，是匹配的
+        for (int i = 1; i <= pLen; i++) {
+            if (p.charAt(i - 1) != '*') {
+                break;
+            }
+            dp[0][i] = true;
+        }
+        for (int i = 1; i <= sLen; i++) {
+            for (int j = 1; j <= pLen; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    // * --> 0 || * --> 匹配当前位置
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+            }
+        }
+        return dp[sLen][pLen];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
