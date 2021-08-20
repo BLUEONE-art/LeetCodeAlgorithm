346d103862bf1f82032c57eac9db8872a7d11cf6
BLUEONE-art <2191380@mail.dhu.edu.cn>
2021-08-20 08:31:54 +0800

    刷题

--- src/multiThreadCommunication/AtomicIntegerDemo.java
+++ src/multiThreadCommunication/AtomicIntegerDemo.java
@@ -3,34 +3,35 @@ package multiThreadCommunication;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class AtomicIntegerDemo {
-    AtomicInteger num = new AtomicInteger();
+    AtomicInteger num = new AtomicInteger(1);
 
-    // 打印奇数
     public void odd() {
         while (num.get() < 100) {
-            if (num.get() % 2 != 0) {
-                System.out.println("当前num的值为：" + num.get());
-//                num.getAndIncrement();
-                num.compareAndSet(num.get(), num.get() + 1);
+            if ((num.get() & 1) == 1) {
+                // 奇数
+                System.out.println("当前打印的奇数为：" + num.get());
+//                 getAndIncrement()方法虽然会CAS将原始的num+1，但是返回的是var5，是原来的值
+                num.getAndIncrement();
+//                num.compareAndSet(num.get(), num.get() + 1);
             }
         }
     }
 
-    // 打印偶数
     public void even() {
         while (num.get() < 100) {
-            if (num.get() % 2 == 0) {
-                System.out.println("当前num的值为：" + num.get());
+            if ((num.get() & 1) == 0) {
+                System.out.println("当前打印的偶数为：" + num.get());
                 num.getAndIncrement();
+//                num.compareAndSet(num.get(), num.get() + 1);
             }
         }
     }
 
     public static void main(String[] args) {
         AtomicIntegerDemo atomicIntegerDemo = new AtomicIntegerDemo();
-        Thread thread1 = new Thread(atomicIntegerDemo::odd, "我是奇数线程");
-        Thread thread2 = new Thread(atomicIntegerDemo::even, "我是偶数线程");
-        thread1.start();
-        thread2.start();
+        Thread t1 = new Thread(atomicIntegerDemo::odd, "奇数线程");
+        Thread t2 = new Thread(atomicIntegerDemo::even, "偶数线程");
+        t1.start();
+        t2.start();
     }
 }
